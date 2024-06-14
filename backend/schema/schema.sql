b8fa1a85435764cd7d63826ef1cef4dfca3d89ea
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-06-14 14:00:48 +0530

    Update database schema

--- backend/schema/schema.sql
+++ backend/schema/schema.sql
@@ -3,7 +3,29 @@
 --
 
 -- Dumped from database version 15.3
--- Dumped by pg_dump version 15.3
+-- Dumped by pg_dump version 15.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: ieeecs; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE ieeecs WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'English_India.1252';
+
+
+ALTER DATABASE ieeecs OWNER TO postgres;
+
+\connect ieeecs
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,6 +41,43 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: events; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.events (
+    id text NOT NULL,
+    name text,
+    description text,
+    date date,
+    budget bigint,
+    "createdAt" timestamp without time zone DEFAULT now(),
+    "createdBy" bigint,
+    closed boolean DEFAULT false
+);
+
+
+ALTER TABLE public.events OWNER TO postgres;
+
+--
+-- Name: invoices; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.invoices (
+    id text NOT NULL,
+    "fileUrl" text,
+    amount bigint,
+    "createdAt" timestamp without time zone,
+    "createdBy" bigint,
+    accepted boolean,
+    "acceptedBy" bigint,
+    "acceptedAt" timestamp without time zone,
+    "eventId" text
+);
+
+
+ALTER TABLE public.invoices OWNER TO postgres;
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
@@ -40,11 +99,51 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: invoices invoices_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.invoices
+    ADD CONSTRAINT invoices_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: invoices invoices_acceptedBy_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.invoices
+    ADD CONSTRAINT "invoices_acceptedBy_fkey" FOREIGN KEY ("acceptedBy") REFERENCES public.users(id);
+
+
+--
+-- Name: invoices invoices_createdBy_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.invoices
+    ADD CONSTRAINT "invoices_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES public.users(id);
+
+
+--
+-- Name: invoices invoices_eventId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.invoices
+    ADD CONSTRAINT "invoices_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES public.events(id);
 
 
 --
