dc1adee32026b32c73e00d3e5addbc0e7ffa98ef
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-06-23 12:16:52 +0530

    Fix

--- frontend/components/InvoiceCard.jsx
+++ frontend/components/InvoiceCard.jsx
@@ -37,7 +37,7 @@ export function InvoiceCard({ invoice }) {
   };
 
   return (
-    <TouchableOpacity onPress={() => router.push('pendingInvoice/view-invoice?fileUrl=' + fileUrl)}>
+    <TouchableOpacity onPress={() => router.navigate('pendingInvoice/view-invoice?fileUrl=' + fileUrl)}>
       <View className=" flex flex-row justify-between items-center bg-darkgray p-4 rounded-3xl mb-4">
         <View className=" flex flex-row items-center">
           {pfp ? <Image source={{
